ab5646eca749133ad1cf8259e1df51aae630bb87
IliaVetchinin <ivetchinin@gmail.com>
2020-11-09 23:15:58 +0300

    Views myshop done

--- myshop/views.py
+++ myshop/views.py
@@ -6,10 +6,46 @@ from .settings.base import INFO
 
 # Create your views here.
 
-class IndexView(LoginRequiredMixin, View):
+class IndexView(View):
     def get(self, request):
-        context = INFO # Должен быть словарём
+        context = INFO  # Должен быть словарём
         return render(request, 'myshop/index.html', context)
+
+
+class AboutView(View):
+    def get(self, request):
+        context = INFO
+        return render(request, 'myshop/about.html', context)
+
+
+class BlogView(View):
+    def get(self, request):
+        context = INFO
+        return render(request, 'myshop/blog.html', context)
+
+
+class CartView(View):
+    def get(self, request):
+        context = INFO
+        return render(request, 'myshop/cart.html', context)
+
+
+class ContactView(View):
+    def get(self, request):
+        context = INFO
+        return render(request, 'myshop/contact.html', context)
+
+
+class ProductSingleView(View):
+    def get(self, request):
+        context = INFO
+        return render(request, 'myshop/product-single.html', context)
+
+
+class WishListView(View):
+    def get(self, request):
+        context = INFO
+        return render(request, 'myshop/wishlist.html', context)
 
 
 class ShopViews(View):
