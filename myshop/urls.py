77a54c62c9b6ae3eab5486e7113e3fc34501371a
IliaVetchinin <ivetchinin@gmail.com>
2020-11-09 23:23:51 +0300

    urls myshop done

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import IndexView, ShopViews
+from .views import IndexView, ShopViews, AboutView, BlogView, CartView
+from .views import ContactView, ProductSingleView, WishListView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
-    path('shop/', ShopViews.as_view(), name='shop')
+    path('shop/', ShopViews.as_view(), name='shop'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('product/', ProductSingleView.as_view(), name='product'),
+    path('wishlist/', WishListView.as_view(), name='wishlist')
 ]
